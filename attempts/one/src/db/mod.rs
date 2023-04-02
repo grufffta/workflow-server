8c24bc1f869710ea8a0157efea445f80d7f0e3e4
Gareth Barlow <gruff@apily.co.uk>
2023-04-02 09:49:36 +0100

    remove previous attempts

--- attempts/one/src/db/mod.rs
+++
@@ -1,8 +0,0 @@
-use std::error::Error;
-
-use clap::__macro_refs::once_cell::sync::OnceCell;
-use surrealdb::Datastore;
-
-pub static DB: OnceCell<Datastore> = OnceCell::new();
-
-pub async fn init() -> Result<(), Error> {}
